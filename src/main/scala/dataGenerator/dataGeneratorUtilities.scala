39140169e90faed7de6f6fa54f2e3de66fa3e49f
cjchz2 <38957891+cjchz2@users.noreply.github.com>
2022-10-10 19:50:01 -0500

    Turned dataGeneratorUtilities into a class

--- src/main/scala/dataGenerator/dataGeneratorUtilities.scala
+++ src/main/scala/dataGenerator/dataGeneratorUtilities.scala
@@ -6,30 +6,27 @@ import org.apache.commons.math3
 import java.io.{File, PrintWriter}
 
 
-object dataGeneratorUtilities extends App {
+class dataGeneratorUtilities(dataGenerationParmList: List[dataGenerationParameters], errorTermParmsVal: errorTermParms,
+                             numberOfRows: Int, headers:String,  fileName:String) {
 
   def generateNormalDistInteger(mean: Int, stdDev: Int): Int =
     new math3.distribution.NormalDistribution(mean, stdDev).sample.toInt
 
-
   def generateUniformDistInteger(lowerBound: Int, upperBound: Int): Int =
     new math3.distribution.UniformIntegerDistribution(lowerBound, upperBound).sample
 
-
-  def generateInputVarAndScaledInputVar(dataGenerationValues: dataGenerationParameters): inputVarAndScaledInputVar =
-    dataGenerationValues match {
+  def generateInputVarAndScaledInputVar(dataGenerationParms: dataGenerationParameters): inputVarAndScaledInputVar =
+    dataGenerationParms match {
       case uniformDistributionVariableParameters(name, lowerBound, upperBound, coefficient) =>
         val inputVar = generateUniformDistInteger(lowerBound, upperBound)
         inputVarAndScaledInputVar(name, inputVar, inputVar * coefficient)
     }
 
+  def generateErrorTerm: Int =
+    generateNormalDistInteger(errorTermParmsVal.mean, errorTermParmsVal.stdDev)
 
-  def generateErrorTerm(errorParmsVal: errorTermParms): Int =
-    generateNormalDistInteger(errorParmsVal.mean, errorParmsVal.stdDev)
-
-
-  def generateRowOfInputs(parmList: List[dataGenerationParameters]): List[inputVarAndScaledInputVar] =
-    parmList.map(generateInputVarAndScaledInputVar)
+  def generateRowOfInputs: List[inputVarAndScaledInputVar] =
+    dataGenerationParmList.map(generateInputVarAndScaledInputVar)
 
   def generateOutputValue(inputVals: List[inputVarAndScaledInputVar], errorTerm: Int): Int =
     inputVals.map(_.scaledInputVar).sum + errorTerm
@@ -38,13 +35,13 @@ object dataGeneratorUtilities extends App {
     val inputList = inputVals.map(_.inputVar)
     inputList :+ outputVal
 
-  def generateManyInputRows(distributionParmList: List[dataGenerationParameters], errorTermParmsVal: errorTermParms, numberOfRows: Int): List[List[Int]] =
-    val listOfInputVars: List[List[inputVarAndScaledInputVar]] = List.fill(numberOfRows)(generateRowOfInputs(distributionParmList))
-    val listOfOutputVars: List[Int] = listOfInputVars.map(generateOutputValue(_, generateErrorTerm(errorTermParmsVal)))
+  def generateManyInputRows: List[List[Int]] =
+    val listOfInputVars: List[List[inputVarAndScaledInputVar]] = List.fill(numberOfRows)(generateRowOfInputs)
+    val listOfOutputVars: List[Int] = listOfInputVars.map(generateOutputValue(_, generateErrorTerm))
     val listOfRows: List[List[Int]] = (listOfInputVars zip listOfOutputVars).map(generateRow(_, _))
     listOfRows
 
-  def writeRowsToCSV(listOfRows:List[List[Int]], headers:String,  fileName:String)=
+  def writeRowsToCSV(listOfRows:List[List[Int]]):Unit =
     val writer = new PrintWriter(new File(fileName))
 
     writer.write(headers)
@@ -55,12 +52,9 @@ object dataGeneratorUtilities extends App {
       writer.write("\n")
     }
     writer.close
-  def generateManyRowsAndWriteToCSV(distributionParmList: List[dataGenerationParameters],
-                                    errorTermParmsVal: errorTermParms, numberOfRows: Int,
-                                    headers:String,  fileName:String
-                                   ):Unit =
-    val listOfRows:List[List[Int]]=  generateManyInputRows(distributionParmList, errorTermParmsVal, numberOfRows)
-    writeRowsToCSV(listOfRows, headers, fileName)
+  def generateManyRowsAndWriteToCSV:Unit =
+    val listOfRows = generateManyInputRows
+    writeRowsToCSV(listOfRows)
 
   
 }
