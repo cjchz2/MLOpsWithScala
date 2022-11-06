ad1184157a488f56a9b4a6815439e9762816802b
cjchz2 <38957891+cjchz2@users.noreply.github.com>
2022-11-06 12:04:15 -0600

    trying to rework csv writer so that module is more testable

--- src/main/scala/dataGeneratorPackage/dataGenerator.scala
+++ src/main/scala/dataGeneratorPackage/dataGenerator.scala
@@ -1,39 +1,49 @@
 package dataGeneratorPackage
 import dataGeneratorPackage.dataGeneratorCaseClasses.*
-
 import org.apache.commons.math3
 
 import java.io.{File, PrintWriter}
+import org.apache.commons.math3.distribution.{AbstractRealDistribution, NormalDistribution}
+import org.apache.commons.math3.random.RandomDataGenerator
 
-class dataGeneratorUtilities(dataGenerationParmList: List[dataGenerationParameters], errorTermParmsVal: errorTermParms,
-                             var totalNumberOfRows: Int, headers:String, filePath:String, baseFileName:String) {
+class invalidBatchSize(s: String) extends Exception(s)
+class invalidRowSize(s: String) extends Exception(s)
 
-  class invalidBatchSize(s: String) extends Exception(s) {}
-  
+class randomDataGenerator extends RandomDataGenerator
+class randomDataGeneratorWriter(fileName:File) extends PrintWriter(fileName:File)
+//class randomDataGeneratorNIOFile extends Files
+
+class dataGenerator(dataGenerationParmList: List[dataGenerationParameters], errorTermParmsVal: errorTermParms,
+                    var totalNumberOfRows: Int, headers:String, filePath:String, baseFileName:String,
+                    val randomDataGeneratorVal: RandomDataGenerator = new randomDataGenerator
+                   ) {
+
   require (totalNumberOfRows > 0)
 
   private var batchSize = 10000
 
+  def generateUniqueFileName: String =
+    filePath +  baseFileName + System.nanoTime()/1000 + ".csv"
+  
   def setBatchSize(newBatchSize:Int): Unit =
     if (newBatchSize > 0)
       batchSize = newBatchSize
     else
       throw new invalidBatchSize("batchSize must be strictly larger than 0")
 
+  def getBatchSize = batchSize
+
   def setTotalNumberOfRows(newTotalNumberOfRows:Int): Unit =
     if (newTotalNumberOfRows > 0)
       totalNumberOfRows = newTotalNumberOfRows
     else
-      println("Row size must be larger than zero")
+      throw new invalidRowSize("totalNumberOfRows must be larger than zero")
 
   def generateNormalDistInteger(mean: Int, stdDev: Int): Int =
-    math3.distribution.NormalDistribution(mean, stdDev).sample.toInt
+    math.ceil(randomDataGeneratorVal.nextGaussian(mean, stdDev)).toInt
 
   def generateUniformDistInteger(lowerBound: Int, upperBound: Int): Int =
-    math3.distribution.UniformIntegerDistribution(lowerBound, upperBound).sample
-
-  def generateErrorTerm: Int =
-    generateNormalDistInteger(errorTermParmsVal.mean, errorTermParmsVal.stdDev)
+    math.ceil(randomDataGeneratorVal.nextUniform(lowerBound, upperBound)).toInt
 
   def generateInputVarAndScaledInputVar(dataGenerationParms: dataGenerationParameters): inputVarAndScaledInputVar =
     dataGenerationParms match {
@@ -42,7 +52,7 @@ class dataGeneratorUtilities(dataGenerationParmList: List[dataGenerationParamete
         inputVarAndScaledInputVar(name, inputVar, inputVar * coefficient)
     }
 
-  def generateRowOfInputs: List[inputVarAndScaledInputVar] =
+  def generateRowOfInputs(dataGenerationParmList: List[dataGenerationParameters]): List[inputVarAndScaledInputVar] =
     dataGenerationParmList.map(generateInputVarAndScaledInputVar)
 
   def generateOutputValue(inputVals: List[inputVarAndScaledInputVar], errorTerm: Int): Int =
@@ -53,14 +63,12 @@ class dataGeneratorUtilities(dataGenerationParmList: List[dataGenerationParamete
     inputList :+ outputVal
 
   def generateBatchOfRows(numberOfRowsToWrite: Int): List[List[Any]] =
-    val listOfInputVars: List[List[inputVarAndScaledInputVar]] = List.fill(numberOfRowsToWrite)(generateRowOfInputs)
-    val listOfOutputVars: List[Int] = listOfInputVars.map(generateOutputValue(_, generateErrorTerm))
+    val listOfInputVars: List[List[inputVarAndScaledInputVar]] = List.fill(numberOfRowsToWrite)(generateRowOfInputs(dataGenerationParmList))
+    val listOfOutputVars: List[Int] = listOfInputVars.map(generateOutputValue(_, generateNormalDistInteger(errorTermParmsVal.mean, errorTermParmsVal.stdDev)))
     val listOfRows: List[List[Any]] = (listOfInputVars zip listOfOutputVars).map(generateRow(_, _))
     listOfRows
 
-  def writeRowsToCSV(listOfRows:List[List[Any]], fileName: String): Unit =
-    val writer = new PrintWriter(new File(fileName))
-
+  def writeRowsToCSV(listOfRows:List[List[Any]], writer: randomDataGeneratorWriter): Unit =
     writer.write(headers)
     writer.write("\n")
 
@@ -76,18 +84,18 @@ class dataGeneratorUtilities(dataGenerationParmList: List[dataGenerationParamete
       else
         batchSize
 
-  def generateUniqueFileName: String =
-    filePath +  baseFileName + System.nanoTime()/1000 + ".csv"
 
-  def generateBatchOfRowsAndApplyFunction(remainingRowsToWrite:Int, functionToApply: (List[List[Any]], String) => Any): Any =
+  def generateBatchOfRowsAndWriteToCSV(remainingRowsToWrite:Int): Any =
     if (remainingRowsToWrite  > 0)
       val numberOfRowsToWrite = returnNumberOfRowsToWrite(remainingRowsToWrite)
       val batchOfRows = generateBatchOfRows(numberOfRowsToWrite)
-      functionToApply(batchOfRows, generateUniqueFileName)
+      val fileName = generateUniqueFileName
+      val writer = new randomDataGeneratorWriter(new File(fileName))
+      writeRowsToCSV(batchOfRows, writer)
       val newRemainingRowsToWrite = remainingRowsToWrite - batchSize
-      generateBatchOfRowsAndApplyFunction(newRemainingRowsToWrite, functionToApply)
+      generateBatchOfRowsAndWriteToCSV(newRemainingRowsToWrite)
 
   def generateAllRowsAndWriteToCSV: Unit =
-    generateBatchOfRowsAndApplyFunction(totalNumberOfRows, writeRowsToCSV)
+    generateBatchOfRowsAndWriteToCSV(totalNumberOfRows)
 
 }
