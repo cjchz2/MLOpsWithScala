8dbba859f9914ad11ae28e817dd402258a939608
cjchz2 <38957891+cjchz2@users.noreply.github.com>
2022-11-14 22:01:31 -0600

    further improvements on applying integer processing

--- src/main/scala/featureCalcPackage/featureCalculations.scala
+++ src/main/scala/featureCalcPackage/featureCalculations.scala
@@ -14,15 +14,9 @@ class featureCalculations {
   def returnDataAsColumns(dataSet: List[List[Any]]): List[List[Any]] =
     ???
 
-object featureCalc extends App {
-//  val fcVal = new featureCalculations
-//  val listVal = fcVal.returnDataFromFileAsRows("C:\\MLOpsFromScratch\\data\\generatedInputData\\houseDataInput640956142740.csv")
-//  println(listVal(0))
-//  val l = List(1,2,3)
-//  print(l.length)
-}
 
-object featureCalculations  extends App {
+
+object featureCalculationsObj  extends App {
   val l = List(List("1,2,3,4"), List("4,5,7,11"), List("10, 11, 12, 13"))
 
   def splitStringRowIntoElements(listOfStringRows: List[List[String]]): List[List[String]] =
@@ -30,8 +24,6 @@ object featureCalculations  extends App {
 
   def returnColumnarFromRowData(listOfSeperatedStringRow: List[List[String]]): List[List[String]] =
     listOfSeperatedStringRow.transpose
-
-  var schema = List(sqft, schoolRating, numberOfBedRooms, price)
 
   def imposeSchema(schema: List[dataVariable], ColumnStringData: List[List[String]]) =
     for (dataVar <- schema) {
@@ -41,56 +33,48 @@ object featureCalculations  extends App {
   var numberVarList = List(1,2,3,4,5)
 
   //How can I get this to work for any "numeric" data type. Things you can add,divide, etc.
-  def standardizeNumericData(columnOfData: List[Int]): List[Double] =
+  def standardizeNumericData(columnOfData: List[Int]): List[Any] =
     val meanOfData = columnOfData.sum/columnOfData.length // V SIZE?
     val deviationFromMean = columnOfData.map(_ - meanOfData)
     val sumOfSquaredDeviations = deviationFromMean.map(scala.math.pow(_,2)).sum
     val variance = sumOfSquaredDeviations/(columnOfData.length-1)
     val standardDeviation = scala.math.sqrt(variance)
-    val standardizedList = numberVarList.map(_ - meanOfData).map(_/standardDeviation)
+    val standardizedList = deviationFromMean.map(_/standardDeviation)
     standardizedList
 
   var dumbData: List[List[Any]] = List(List("1", "2", "3","4","5"),List("not", "a", "number"))
   var dumbSchema: List[List[Any]] = List(List("Int",0), List("String",1))
-//  dumbData.foreach(println)
 
-  for (schema <- dumbSchema) {
-//    println(schema(1).asInstanceOf[Int])
-    if (schema(0).asInstanceOf[String] == "Int") {
-      println("This is a Int column")
-      val intStringDumbCol = dumbData(schema(1).asInstanceOf[Int])
-      val intDumbCol = intStringDumbCol.map(_.asInstanceOf[String].toInt)
-      val standardizedIntDumbCol = standardizeNumericData(intDumbCol)
-      println(standardizedIntDumbCol)
-    }
-//    println(dumbData(schema(1).asInstanceOf[Int]))
-  }
+  def applyIntegerDataProcessing(columnOfData:List[Any], schema: List[Any]): List[Any] =
+      if (schema(0).asInstanceOf[String] == "Int") {
+        val stringCol = columnOfData.asInstanceOf[List[String]]
+        val intCol = stringCol.map(_.toInt)
+        val standardizedIntCol = standardizeNumericData(intCol)
+        standardizedIntCol
+      }
+      else {
+        columnOfData
+      }
+  def applyIntegerDataProcessingAlt(dataAndSchema:Tuple2[List[Any],Any])= //: List[Any] =
+    if (dataAndSchema(1) == "Int")
+      standardizeNumericData(dataAndSchema(0).map(_.toString).map(_.toInt))
+    else
+      dataAndSchema(0)
+//      val intCol =
+//      standardizeNumericData(dataAndSchema(0))
+  val data = List(List("1", "2","3", "4"), List("a", "thing", "would", "cool"))
+  val schema = List("Int","String")
 
 
-//  standardizeNumericData(numberVarList).foreach(println)
-  //  var scala.math.sqrt(sumOfSquaredDeviations)
-//  columnarList.foreach(println)
-//  def returnColumnarData(listOfRows: List[List[Int]]):
-//    listOfRows.transpose
-//  val number_of_rows = l.length
-//  //Weakness here is that the first row needs to have correct number of delimiters
-//  val number_of_columns = l(0)(0).split(",").length
-//  println(number_of_rows)
-//  println(number_of_columns)
-//  var columnarData = List.fill(number_of_rows)(List.fill(number_of_columns))
-//  val seperatedElement = l.map(x => x(0).split(",").toList)
-//  seperatedElement.foreach(println)
-//  var newList = List.empty[List[Any]]
-//  val listSep = l(0)(0).split(",")
-//  listSep.foreach(println)
-//  for (rowInList <- l) {
-//    for (rowAsString <- rowInList) {
-//      val splitElements = rowAsString.split(",").toList
-//      println(rowInList)
-////      print(splitElements)
-////      newList = splitElements :: newList
-//    }
-//  }
+  val dataAndSchema = data.zip(schema)
+  dataAndSchema.map(applyIntegerDataProcessingAlt(_)).foreach(println)
+
+//  val zippedColSchema: List[(List[String], List[Matchable])] = data.zip(schema)
+//  zippedColSchema.map(applyIntegerDataProcessing((_,_)))
+//  zippedColSchema.foreach(println)
+//  val columnOfData = List(1,2,3,4)
+//  println(columnOfData.sum/columnOfData.length)
+
 }
 
   // for (line <- newList) {
