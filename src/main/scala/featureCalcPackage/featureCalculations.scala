87417191a6b6e2705b9b09d1e3b23f131f3f152b
cjchz2 <38957891+cjchz2@users.noreply.github.com>
2022-11-20 10:42:10 -0600

    Rough version that reads in CSV, applies a basic feature calc is complete

--- src/main/scala/featureCalcPackage/featureCalculations.scala
+++ src/main/scala/featureCalcPackage/featureCalculations.scala
@@ -2,111 +2,69 @@ package featureCalcPackage
 
 import scala.io.Source
 import dataGeneratorPackage.dataGeneratorCaseClasses._
-class featureCalculations {
-
-  def returnDataFromFileAsRows(fileName: String):List[List[Any]] =
-    var listVal = List.empty[List[Any]]
-    for (line <- Source.fromFile(fileName).getLines)
-//      val cols = line.split(",").map(_.toInt)
-      listVal = List(line) :: listVal
-    listVal
-}
-  def returnDataAsColumns(dataSet: List[List[Any]]): List[List[Any]] =
-    ???
 
 
 
-object featureCalculationsObj  extends App {
-  val l = List(List("1,2,3,4"), List("4,5,7,11"), List("10, 11, 12, 13"))
+/*
+To do.
+1. Read CSVs in batches? Difficult to do since we have to calculate a global mean/std dev across
+whole dataset.
+2. Figure out more elegant solution to schema approach?
+3. Make standardizeNumericData generic to deal with other numeric data types.
+4. How to get returnDataFromFileAsRows to append data vs prepend it?
+5. Unit test
+
+*/
+
+object featureCalculations  extends App {
 
   def splitStringRowIntoElements(listOfStringRows: List[List[String]]): List[List[String]] =
-    listOfStringRows.map(x => x(0).split(",").toList)
+    listOfStringRows.map(x => x.head.split(",").toList)
 
   def returnColumnarFromRowData(listOfSeperatedStringRow: List[List[String]]): List[List[String]] =
     listOfSeperatedStringRow.transpose
 
-  def imposeSchema(schema: List[dataVariable], ColumnStringData: List[List[String]]) =
-    for (dataVar <- schema) {
-      println(dataVar)
-    }
-
-  var numberVarList = List(1,2,3,4,5)
-
   //How can I get this to work for any "numeric" data type. Things you can add,divide, etc.
   def standardizeNumericData(columnOfData: List[Int]): List[Any] =
-    val meanOfData = columnOfData.sum/columnOfData.length // V SIZE?
+    val meanOfData = columnOfData.sum / columnOfData.length // V SIZE?
     val deviationFromMean = columnOfData.map(_ - meanOfData)
-    val sumOfSquaredDeviations = deviationFromMean.map(scala.math.pow(_,2)).sum
-    val variance = sumOfSquaredDeviations/(columnOfData.length-1)
+    val sumOfSquaredDeviations = deviationFromMean.map(scala.math.pow(_, 2)).sum
+    val variance = sumOfSquaredDeviations / (columnOfData.length - 1)
     val standardDeviation = scala.math.sqrt(variance)
-    val standardizedList = deviationFromMean.map(_/standardDeviation)
+    val standardizedList = deviationFromMean.map(_ / standardDeviation)
     standardizedList
 
-  var dumbData: List[List[Any]] = List(List("1", "2", "3","4","5"),List("not", "a", "number"))
-  var dumbSchema: List[List[Any]] = List(List("Int",0), List("String",1))
-
-  def applyIntegerDataProcessing(columnOfData:List[Any], schema: List[Any]): List[Any] =
-      if (schema(0).asInstanceOf[String] == "Int") {
-        val stringCol = columnOfData.asInstanceOf[List[String]]
-        val intCol = stringCol.map(_.toInt)
-        val standardizedIntCol = standardizeNumericData(intCol)
-        standardizedIntCol
-      }
-      else {
-        columnOfData
-      }
-  def applyIntegerDataProcessingAlt(dataAndSchema:Tuple2[List[Any],Any])= //: List[Any] =
-    if (dataAndSchema(1) == "Int")
-      standardizeNumericData(dataAndSchema(0).map(_.toString).map(_.toInt))
+  def applyDataProcessingOnColumn(columnAndSchema: Tuple2[List[String], String]): List[Any] =
+    if (columnAndSchema(1) == "Int")
+      columnAndSchema(0).head :: standardizeNumericData(columnAndSchema(0).tail.map(_.toInt))
     else
-      dataAndSchema(0)
-//      val intCol =
-//      standardizeNumericData(dataAndSchema(0))
-  val data = List(List("1", "2","3", "4"), List("a", "thing", "would", "cool"))
-  val schema = List("Int","String")
+      columnAndSchema(0)
+
+  //How can I append as opposed to prepending? The "::" operator is not allowing these to be swapped even though both are lists.
+  //Getting the following error when swapped Required List[List[String]], required List[Object] (for listVal).
+  //But I am specifying listVal as an empty List[String]!
+  def returnDataFromFileAsRows(fileName: String): List[List[String]] =
+    var listVal = List.empty[List[String]]
+    val dataFile = scala.io.Source.fromFile(fileName)
+    for (line <- dataFile.getLines){
+      listVal =  List(line) :: listVal}
+    dataFile.close()
+    listVal.reverse
+
+  def applyTransformationsToAllData(data: List[List[String]], schema: List[String]):List[List[Any]] =
+    var transformedData = List.empty[List[Any]]
+    val zippedList = data zip schema
+    for (data <- zippedList) {
+      val thing = applyDataProcessingOnColumn(data)
+      transformedData = thing :: transformedData
+    }
+    transformedData
 
 
-  val dataAndSchema = data.zip(schema)
-  dataAndSchema.map(applyIntegerDataProcessingAlt(_)).foreach(println)
+  def readDataCSVAndApplyFeatureCalculations(fileName:String,csvSchema:List[String]) =
+    val csvList = returnDataFromFileAsRows(fileName)
+    val splitCSVList = splitStringRowIntoElements(csvList)
+    val columnarList = returnColumnarFromRowData(splitCSVList)
 
-//  val zippedColSchema: List[(List[String], List[Matchable])] = data.zip(schema)
-//  zippedColSchema.map(applyIntegerDataProcessing((_,_)))
-//  zippedColSchema.foreach(println)
-//  val columnOfData = List(1,2,3,4)
-//  println(columnOfData.sum/columnOfData.length)
-
+    applyTransformationsToAllData(columnarList,csvSchema)
 }
-
-  // for (line <- newList) {
-  ////   println(line)
-  //   for (element <- line) {
-  //     try {
-  //       println(element.asInstanceOf[String].toInt)
-  //     }
-  //     catch {
-  //       case e: Exception => println(element)
-  //     }
-  //   }
-  // }
-
-  //  def toNonAny(x: Any): Any = {
-  //    x match {
-  //      case i: Int => i
-  //      case s: String => s
-  //      case _ => println("Unknown")
-  //    }
-  //  }
-  //  var newestList = List.empty[Int]
-  //  for (line <- newList) {
-  //    //   println(line)
-  //    for (element <- line) {
-  //      println(toNonAny(element))
-  //      newestList= toNonAny(element) :: newestList
-  //    }
-  //  }
-  //  newestList.map(_+1)
-
-
-
-
-//}
--- src/main/scala/featureCalcPackage/test.scala
+++
@@ -1,5 +0,0 @@
-package featureCalcPackage
-
-object test {
-
-}
