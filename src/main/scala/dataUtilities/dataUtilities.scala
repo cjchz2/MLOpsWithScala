7ffac4634da66e7d7ea61dfb5d56b2426285823c
cjchz2 <38957891+cjchz2@users.noreply.github.com>
2022-11-20 19:09:24 -0600

    creating catch all for generic operations

--- src/main/scala/dataUtilities/dataUtilities.scala
+++ src/main/scala/dataUtilities/dataUtilities.scala
@@ -1,5 +1,22 @@
 package dataUtilities
 
-object dataUtilities {
+import com.typesafe.config.{Config, ConfigFactory}
 
+object readingDataFromFlatFile extends App{
+  def returnDataFromFileAsRows(fileName: String): List[List[String]] =
+    var listVal = List.empty[List[String]]
+    val dataFile = scala.io.Source.fromFile(fileName)
+    for (line <- dataFile.getLines) {
+      listVal = List(line) :: listVal
+    }
+    dataFile.close()
+    listVal.reverse
+
+  def splitStringRowIntoElements(listOfStringRows: List[List[String]]): List[List[String]] =
+      listOfStringRows.map(x => x.head.split(",").toList)
+
+  def returnColumnarFromRowData(listOfSeperatedStringRow: List[List[String]]): List[List[String]] =
+    listOfSeperatedStringRow.transpose
 }
+
+
