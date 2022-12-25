c66c1634749f7ccf070f772de348ed2e264cabb5
cjchz2 <38957891+cjchz2@users.noreply.github.com>
2022-12-25 13:14:13 -0600

    wrote function to determine if any given col has drifted with map

--- src/main/scala/dataDrift/dataDriftDetector.scala
+++ src/main/scala/dataDrift/dataDriftDetector.scala
@@ -27,31 +27,14 @@ class dataDriftDetector {
     val ksTest = inference.KolmogorovSmirnovTest()
     trainingAndInput.map(ksTest.kolmogorovSmirnovTest(_, _, false))
 
-  val zipped = combineTrainingAndInputArray
-  performKSTestOnAllColumns(zipped).foreach(println)
+  def minBool(v1: Boolean, v2: Boolean): Boolean = if (v1 < v2) v1 else v2
 
-  //
-  //  def combineDataInDir(dirName: String) =
-  //  for (file <- trainingeDir; if file contains "Feature") {
-  //    readingDataFromFlatFile.returnDataFromFilesAsRowsAndSplitValues(file).tail
-  //
-  //  }
-  //  val listOne: Array[Double] = Array(1,2,3,3,4,5,3,1,3,4,5,2,4,1,3,4,5,6)
-  //  val listTwo: Array[Double] = Array(1000,4,1,1000,2,22345,245,4,5,4,3,55,30,333,23,12,2,5)
-  //  val trainingDataDir = new File("data/trainingData/")
-  //  val trainingFilesNamesAsString: Array[String] = trainingDataDir.listFiles.map(_.toString)
-  //  // Transform targets and features.
-  ////  for (file <- trainingFilesNamesAsString; if file contains "Features") {
-  ////    val transformer = new transformData(file, targetDataProfileFileName)
-  ////    returnDataFromFilesAsRowsAndSplitValues()
-  ////  }
-  //
-  //  def returnKSPValueOnSingleColumn(trainingData: List[List[Double]], incrementalData):Double =
-  //
-  //
-  //  val trainingData = trainingFilesNamesAsString.map((file:String) => readingDataFromFlatFile.returnDataFromFileAsRows(file))
-  ////  val trainingDataNoHeaders = trainingData.map((dataSet: List[List[String]]) => dataSet.tail)
-}
+  def determineRetraining(ksResults: Array[Double]): Boolean =
+    ksResults.map(_ < .05).reduceLeft(minBool)
+
+  val zipped = combineTrainingAndInputArray
+  println(determineRetraining(performKSTestOnAllColumns(zipped)))
+
 
 
 object dataDriftDetector extends App {
